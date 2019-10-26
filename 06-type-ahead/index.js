90edd143e7045df0cb241df9245e713b875c1291
AQadir64 <a.qadir67@outlook.com>
2019-10-26 10:43:30 +0500

    functionality added

--- 06-type-ahead/index.js
+++ 06-type-ahead/index.js
@@ -1,2 +1,45 @@
 const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+
+const promp = fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+
+const findMatches = function(wordToMatch, cities) {
+  return cities.filter(value => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return value.city.match(regex) || value.state.match(regex);
+  });
+};
+
+function displayChnage() {
+  const matchedArray = findMatches(this.value, cities);
+  const html = matchedArray
+    .map(place => {
+      const highlatedName = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        highlatedName,
+        ` <span class="hl">${this.value}</span>`
+      );
+      const stateName = place.state.replace(
+        highlatedName,
+        ` <span class="hl2">${this.value}</span>`
+      );
+      return `<li>
+        <span class ="name">
+          ${cityName} , ${stateName}
+        </span>
+        <span class="population"> ${place.population}</span>
+      </li>`;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+}
+
+const input = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+input.addEventListener("change", displayChnage);
+input.addEventListener("keyup", displayChnage);
